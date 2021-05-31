eb077f04a56c77de764476733bb01950036b8a35
Otoniel Duarte <otonielduarte2@gmail.com>
2021-05-31 11:14:19 -0300

    refactor and create dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:12 as base
+ENV NODE_ENV=development
+WORKDIR /app
+COPY . .
+
+
+FROM base as install
+WORKDIR /app
+RUN yarn
+
+
+FROM install as server
+WORKDIR /app
+COPY --from=dependencies /app/node_modules node_modules/
+CMD [ "yarn dev:server" ]
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
-const http = require('http');
 
-port = process.env.PORT || 3000;
+port = process.env.APP_PORT || 3000;
 
-const server = http.createServer(app);
-server.listen(port);
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server started, listening on port: ${process.env.APP_PORT}`);
+  });
