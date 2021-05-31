675ab451f47136688c3ea3f39443cb1d3d8ec9bd
Rodrigo Bastos <rodrigobastosv@gmail.com>
2021-05-31 17:19:11 -0300

    fake data

---
+++ api/data/products/more_points_products.js
@@ -0,0 +1,47 @@
+export default morePointsProducts = [
+    {
+        "id": 4,
+        "imageUrls": [
+            "https://i.imgur.com/9psrGJK.png",
+            "https://i.imgur.com/9psrGJK.png",
+            "https://i.imgur.com/9psrGJK.png",
+            "https://i.imgur.com/9psrGJK.png"
+        ],
+        "description": "Lava & Seca 11 Kg Samsung IOT Inox com 12 Programas de Lava",
+        "sku": "sku",
+        "sellerName": "Top Store",
+        "price": 3699.8,
+        "fakePrice": 4199,
+        "discount": 8,
+        "points": 11000,
+        "installments": [
+            {
+                "number": 1,
+                "value": 3699.8
+            }
+        ],
+        "variations": []
+    },
+    {
+        "id": 5,
+        "imageUrls": [
+            "https://i.imgur.com/yjR3b2T.png",
+            "https://i.imgur.com/yjR3b2T.png",
+            "https://i.imgur.com/yjR3b2T.png"
+        ],
+        "description": "iPad Air Tela 10,9' 4 ª A A Geração Apple - Wi - Fi + Cellular 64 GB ",
+        "sku": "sku",
+        "sellerName": "Top Store",
+        "price": 4899,
+        "fakePrice": 5499,
+        "discount": 11,
+        "points": 14700,
+        "installments": [
+            {
+                "number": 1,
+                "value": 4899
+            }
+        ],
+        "variations": []
+    }
+];
---
+++ api/data/products/offer_product.js
@@ -0,0 +1,21 @@
+export default offerProduct = {
+    "id": 6,
+    "imageUrls": [
+        "https://i.imgur.com/GnW62sE.png",
+        "https://i.imgur.com/GnW62sE.png"
+    ],
+    "description": "iPhone 12 Azul, Tela de 6,1', 5 G, 128 GB, Câmera Dupla de 12 MP Ultra - angular + 12 MP",
+    "sku": "sku",
+    "sellerName": "Top Store",
+    "price": 5999.2,
+    "fakePrice": 6499,
+    "discount": 11,
+    "points": 2500,
+    "installments": [
+        {
+            "number": 1,
+            "value": 5999.2
+        }
+    ],
+    "variations": []
+};
