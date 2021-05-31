3f575d9035dc216ad86a05211f8fd75bb88c9dd5
Otoniel Duarte <otonielduarte2@gmail.com>
2021-05-31 11:17:30 -0300

    rollback

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const app = require('./app');
+const http = require('http');
 
 port = process.env.APP_PORT || 3000;
 
-app.listen(process.env.APP_PORT, () => {
+const server = http.createServer(app);
+
+server.listen(port,() => {
     console.log(`Server started, listening on port: ${process.env.APP_PORT}`);
   });
