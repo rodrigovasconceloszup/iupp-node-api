a0ab98ef76417024f9e20b7b8c7ad4877c7b4357
Otoniel Duarte <otonielduarte2@gmail.com>
2021-06-07 11:21:12 -0300

    fix shocase uuod

--- api/data/showcase/showcase.ts
+++ api/data/showcase/showcase.ts
@@ -1,43 +1,44 @@
 import allProducts from '../products/all_products';
 import morePointsProducts from '../products/more_points_products';
 import offerProduct from '../products/offer_product';
+import { v4 as uuid } from 'uuid';
 
 const showcase = {
-    "banners": [
-        {
-            "id": 1,
-            "bannerUrl": "https://i.imgur.com/u6NDGqx.png"
-        },
-        {
-            "id": 2,
-            "bannerUrl": "https://i.imgur.com/u6NDGqx.png"
-        },
-        {
-            "id": 3,
-            "bannerUrl": "https://i.imgur.com/u6NDGqx.png"
-        }
-    ],
-    "categorizedProducts": [
-        {
-            "category": "Brand New",
-            "products": [
-                ...allProducts
-            ]
-        },
-        {
-            "category": "More Points",
-            "products": [
-                ...morePointsProducts
-            ]
-        }
-    ],
-    "offers": [
-        {
-            "product": offerProduct,
-            "startDate": "2021-05-27",
-            "endDate": "2021-05-28"
-        }
-    ]
+  "banners": [
+    {
+      "id": uuid(),
+      "bannerUrl": "https://i.imgur.com/u6NDGqx.png"
+    },
+    {
+      "id": uuid(),
+      "bannerUrl": "https://i.imgur.com/u6NDGqx.png"
+    },
+    {
+      "id": uuid(),
+      "bannerUrl": "https://i.imgur.com/u6NDGqx.png"
+    }
+  ],
+  "categorizedProducts": [
+    {
+      "category": "Brand New",
+      "products": [
+        ...allProducts
+      ]
+    },
+    {
+      "category": "More Points",
+      "products": [
+        ...morePointsProducts
+      ]
+    }
+  ],
+  "offers": [
+    {
+      "product": offerProduct,
+      "startDate": "2021-05-27",
+      "endDate": "2021-05-28"
+    }
+  ]
 }
 
 export default showcase;
